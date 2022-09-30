76be1016c9080c8e4ba9135e7d569d98e42e8ea1
siferga <silvia.fernandez.pro@gmail.com>
2022-09-30 14:32:25 +0200

    modif

--- controller/creer_user.php
+++ controller/creer_user.php
@@ -2,25 +2,28 @@
 //session_start();  //$_SESSION 
 //Elle va persister entre 2 variables php
 
-// Formulaire de connexion
-
-
-//Soumission de formulaire
+// Traitement du formulaire de connexion
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $usersDao = new UsersDAO(); // connexion bdd
 
-    $user = new User(null, $_POST['email'], $_POST['password']);
+    $user = new Users($_POST['email'], $_POST['password']);
     //création user
-
+    session_start();
+    $_SESSION['date'] = date('\l\e d/m/Y à H:i:s');
+    $_SESSION['email'] = $email;
+    // Puis rediriger l'utilisateur vers la page d'accueil 
+    // en appelant la fonction générique url() pour être
+    // certain que l'identifiant de session est transmis
+    // quelles que soient les conditions. $addUser = $usersDao->getOne($_POST['email']) && isset($_POST['password']);
 
     $status = $usersDao->add($user); // appelle contrôleur add pour ajouter un utilisateur
 
     //On affiche le template Twig correspondant
     if ($status) {
-        echo $twig->render('creer_user.html.twig', ['status' => "Ajout OK", 'utilisateur' => $user]);
+        echo $twig->render('login.html.twig', ['status' => "Ajout OK", 'user' => $user]);
     } else {
-        echo $twig->render('creer_user.html.twig', ['status' => "Erreur Ajout"]);
+        echo $twig->render('login.html.twig', ['status' => "Erreur Ajout"]);
     }
 } else { // on affiche le twig avec le formulaire pour ajouter l'film
     echo $twig->render('creer_user.html.twig');
--- controller/login.php
+++ controller/login.php
@@ -1,13 +1,30 @@
 <?php
 
 //Soumission de formulaire
-
 $usersDao = new UsersDAO(); // connexion bdd
 
-//création user
 if (isset($_POST['email']) && isset($_POST['password'])) {
-    $addUser = $usersDao->getOne($_POST['email']) && isset($_POST['password']);
-} else { // login user
+
+    /*foreach ($users as $user) {
+        //utilisateur trouvée
+    }*/
+
+
+    $user = new Users(null, $_POST['email'], $_POST['password']);
+    //création user
+
+    $status = $usersDao->addUser($user); // appelle contrôleur add pour ajouter une film
+
     //On affiche le template Twig correspondant
+    if ($status) {
+        echo $twig->render('login.html.twig', ['status' => "Ajout OK", 'user' => $user]);
+    } else {
+        echo $twig->render('login.html.twig', ['status' => "Erreur Ajout"]);
+    }
+} else { // on affiche le twig avec le formulaire pour ajouter un nouveau utilisateur
     echo $twig->render('login.html.twig');
 }
+
+
+//la session de l'usuaire reste activée 
+//if (!isset($_SESSION['LOGGED_USER']));
