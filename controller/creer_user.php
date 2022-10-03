699dcd9be2c10873916ca48a47422d30ed315893
siferga <silvia.fernandez.pro@gmail.com>
2022-10-03 13:46:46 +0200

    modifs user

--- controller/creer_user.php
+++ controller/creer_user.php
@@ -1,54 +1,21 @@
 <?php
-if (isset($_POST['email']))
-{
-    // Récupérer les information saisies.
-    $email = sanitizeString($_POST['email']);
-    $password = sanitizeString($_POST['password']);
-    // Vérifier que l'utilisateur existe.
-
-    if ($email == "" || $password == "") {
-        $error = 'Tous les champs n\'ont pas éré saisies';
-    } else {
-         
-        $result = queryMySQL("SELECT email, password FROM user 
-        WHERE email = '$email AND password = $password);
-       
-        if ($result->rowCount())
-        { 
-            $error  = 'Ce email existe déjà<br><br>';
-            /*Afficher le formulaire/template Twig correspondant*/
-            echo $twig->render('login.html.twig');
-        }
-        else
-        /* Vérifier que l'utilisateur existe= ouvrir session et
-        enregistrer les données de session. */
-        {
-            $result = queryMySQL("INSERT INTO user VALUES ('$email','$password')");
-            die ('h4Compte créé>/h4>Veuillez vous connecter. >/div></body></html>
-    } 
-
-}
-
-session_start();  //$_SESSION 
-//Elle va persister entre 2 variables php
 
 // Traitement du formulaire de connexion
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
-    $usersDao = new UsersDAO(); // connexion bdd
+    $usersDao = new UserDAO(); // connexion bdd
 
-    $user = new Users($_POST['email'], $_POST['password']);
+    $user = new User($_POST['email'], $_POST['password']);
     //création user
-    
+
     $status = $usersDao->add($user); // appelle contrôleur add pour ajouter un utilisateur
 
     //On affiche le template Twig correspondant
     if ($status) {
-        echo $twig->render('login.html.twig', ['status' => "Ajout OK", 'user' => $user]);
+        echo $twig->render('creer_user.html.twig', ['status' => "Ajout OK", 'user' => $user]);
     } else {
-        echo $twig->render('login.html.twig', ['status' => "Erreur Ajout"]);
+        echo $twig->render('creer_user.html.twig', ['status' => "Erreur Ajout"]);
     }
 } else { // on affiche le twig avec le formulaire pour ajouter l'film
     echo $twig->render('creer_user.html.twig');
 }
-
--- controller/login.php
+++ controller/login.php
@@ -1,75 +1,43 @@
 <?php
-require_once 'header.php';
 //Soumission de formulaire
-$usersDao = new UsersDAO(); // connexion bdd
+$userDao = new UserDAO(); // connexion bdd
 
 // Initialisation des variables.
-$email = $password = $error = '';
+$email = "";
+$password = "";
 // Traitement du formulaire.
-if (isset($_POST['email'])) /*(isset($_POST['password']))*/
-{
+if (isset($_POST['connexion'])) /*(isset($_POST['password']))*/ {
     // Récupérer les information saisies.
+    
     //  sanitizeString supprie tout code malintentionné eventuel et toute balise interdite d'une entrée de l'utilisateur.
-    $email = sanitizeString($_POST['email']);
-    $password = sanitizeString($_POST['password']);
+    //$email = sanitizeString($_POST['email']);
+    //$password = sanitizeString($_POST['password']);
     // Vérifier que l'utilisateur existe.
 
     if ($email == "" || $password == "") {
-        $error = 'Tous les champs n\'ont pas éré saisies';
+        //Tous les champs n\'ont pas été saisies';
+
+
     } else {
         //queryMySQL envoie une requete (query) à MySQL et produit un message d'erreur en cas d'échec.
-        $result = queryMySQL("SELECT email, password FROM user 
-        WHERE email = '$email AND password = $password);
+        $result = queryMySQL("SELECT email, password FROM user WHERE email = '$email AND password = $password'");
        
         if ($result->rowCount() == 0);
         { 
-            $error  = 'Utilisateur non enregistré ou mot de pass non valide';
-            $error .= 'Enregistrez-vous.';
-            /*Afficher le formulaire/template Twig correspondant*/
+            //Utilisateur non enregistré ou mot de pass non valide
             echo $twig->render('creer_user.html.twig');
-        } else
-        /* Vérifier que l'utilisateur existe= ouvrir session et
-        enregistrer les données de session. */
-        {
-        /*session_start();*/
-        $_SESSION[email] = $email;
-        $_SESSION[password] = $password;
-        echo $twig->render('accueil.html.twig');
-        die (  'Vous estes connecté. Cliquez ici pour continuer' );
-        /*Puis rediriger l'utilisateur vers la page d'accueil 
+        } else {
+        /*L'email existe déjà, ouvrir session et enregistrer les données */
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+        /*Rediriger l'utilisateur vers la page d'accueil */
+        /*Afficher le formulaire/template Twig correspondant*/
+      
+        echo $twig->render('rechercher_films.html.twig');
+
+        /*Rediriger l'utilisateur vers la page d'accueil 
         en appelant la fonction générique url() pour être
         certain que l'identifiant de session est transmis
         quelles que soient les conditions.*/
     }
-       /* header('location: ' . ('/login.php'));
-        exit;*/
-    } 
-     
-
 }
-
-/*//la session de l'usuaire reste activée 
-if (!isset($_SESSION[LOGGED_USER]));*/
-
-/*if (isset($_POST[email]) && isset($_POST[password])) {
-
-    $user = new Users(null, $_POST[email], $_POST[password]);
-    //création user
-    session_start();
-    $_SESSION[email] = $email;
-    // Puis rediriger l'utilisateur vers la page d'accueil 
-    // en appelant la fonction générique url() pour être
-    // certain que l'ide ntifiant de session est transmis
-    // quelles que soient les conditions. $addUser = $usersDao->getOne($_POST[email]) && isset($_POST[password]);
-
-    $status = $usersDao->add($user); // appelle contrôleur add pour ajouter une film
-
-    //On affiche le template Twig correspondant
-    if ($status) {
-        echo $twig->render('login.html.twig', ['status' => 'Ajout OK', 'user' => $user]);
-    } else {
-        echo $twig->render('login.html.twig', ['status' => 'Erreur Ajout']);
-    }
-} else { // on affiche le twig avec le formulaire pour ajouter un nouveau utilisateur
-    echo $twig->render('creer_user.html.twig');
-}*/
