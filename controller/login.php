0c884d6357d25e5f420dac29e0c2bed01120722a
Emy-05 <emili32510@gmail.com>
2022-10-05 14:48:05 +0200

    bug fixes login

--- controller/login.php
+++ controller/login.php
@@ -1,9 +1,14 @@
 <?php
 //isset = verifier email et password sont rentrés dans le formulaire
 if (isset($_POST["email"]) && isset($_POST["password"])) {
+    $emailUser = $_POST["email"];
+    $passwordHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    // on hashe le password reçu par formulaire qu'on récupère dans une variable
+
     $userDao = new UsersDAO(); //connexion dbb
     //Cherche fonction get_user dans UserDAO 
     $user = $userDao->get_user(($_POST["email"]));
+
     // Si user exist nous recuperons les getters
     if ($user != null) {
         $userEmail = $user->get_email();
@@ -11,16 +16,24 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         $userName = $user->get_userName();
         $idUser = $user->get_idUser();
         // Vérifier que l'email et le password correspondent à un utilisateur.
-        if (($userEmail == $_POST["email"]) && ($userPassword == $_POST["password"])) {
+        if (($userEmail == $_POST["email"])) {
             $_SESSION['email'] = $userEmail;
             $_SESSION['userName'] = $userName;
             $_SESSION['idUser'] = $idUser;
+            $_SESSION['password'] = $password;
+
+            if (password_verify($userPassword, $passwordHash)) {
+                echo "connexion réussie";
+            } else {
+                echo "identifiants invalides";
+            }
+
             //Si le Remember me est coché
             if (isset($_POST['remember'])) {
                 setcookie($userEmail, $UserPassword);
             }
 
-            header('location:rechercher_film');
+            header('location:rechercher_films');
         } else {
             echo $twig->render('login.html.twig', ['password' => 'Pas de connexion.']);
         }
